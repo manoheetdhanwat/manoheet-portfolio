2305da48d41c7dfd92c789db38dbc1348634602f
manoheetdhanwat <omdhanwat368@gmail.com>
2026-03-19 20:58:56 +0530

    Fix TypeScript errors in splitText and add GSAP trial type declarations

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -1,12 +1,2 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
-import { SplitText } from "gsap-trial/SplitText";
-
-interface ParaElement extends HTMLElement {
-  anim?: gsap.core.Animation;
-  split?: SplitText;
-}
-
 export default function setSplitText() {
 }
